2aa8e478a2e1dcf2f7412377eafcd69a48d234aa
Ernesto Chero <ernesto@jemstep.com>
2019-10-09 17:16:13 -0500

    addding sdk-vertx-client 0.13.0-SNAPSHOT to sbt

--- build.sbt
+++ build.sbt
@@ -4,6 +4,6 @@ version := "0.1"
 
 scalaVersion := "2.13.1"
 
-libraryDependencies += "io.nem" % "sdk" % "0.9.1"
-libraryDependencies += "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.10.0"
-libraryDependencies += "com.fasterxml.jackson.core" % "jackson-databind" % "2.10.0"
+resolvers += "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
+
+libraryDependencies += "io.nem" % "sdk-vertx-client" % "0.13.0-SNAPSHOT" changing()
--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -1,11 +1,13 @@
-import io.nem.sdk.infrastructure.AccountHttp
+import io.nem.sdk.infrastructure.vertx.AccountRepositoryVertxImpl
 import io.nem.sdk.model.account.Address
+import io.nem.sdk.model.account.Account
+import io.nem.sdk.openapi.vertx.invoker.ApiClient
 object App {
   def main(args: Array[String]): Unit = {
-    val accountHttp = new AccountHttp("http://localhost:3000")
+    //val accountHttp = new AccountRepositoryVertxImpl(new ApiClient())
     val address =
       Address.createFromRawAddress("SAJPSBGA7CWXUMGWIS2JSY6MDG5U26BY4RL65QTS")
-    val accountInfo = accountHttp.getAccountInfo(address)
-    accountInfo.subscribe(in => println(s"information : ${in}"))
+    /* val accountInfo = accountHttp.getAccountInfo(address)
+    accountInfo.subscribe(in => println(s"information : ${in}"))*/
   }
 }
